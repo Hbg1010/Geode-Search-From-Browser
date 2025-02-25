fea71ed470344da8b7abcf1d37d359ffc787cab8
hbg1010 <harrisong26@parkschool.net>
2025-02-24 22:26:39 -0500

    popups work now!

--- src/main.cpp
+++ src/main.cpp
@@ -14,19 +14,8 @@
 
 using namespace geode::prelude;
 
-// class LevelBrowserLayerFromSearch : public LevelBrowserLayer {
-// 	void keyBackClicked() override {
-// 		if (true) {
-// 			CCDirector::sharedDirector()->popScene();
-// 		} else {
-// 			LevelBrowserLayer::keyBackClicked();
-// 		}
-// 	}
-// };
-
-void addSearchLayer(GJSearchObject* searchObj) {
-	auto levelLayer = LevelBrowserLayer::scene(searchObj);
-
+void addSearchLayer(SearchType type, std::string input) {
+ 
 	if (auto x = typeinfo_cast<PlayLayer*>(CCScene::get()->getChildren()->objectAtIndex(0))) {
 		if (!x->m_isPaused) x->pauseGame(true);
 			bool shouldReplace = false;
@@ -34,9 +23,13 @@ void addSearchLayer(GJSearchObject* searchObj) {
 			"HOLD ON!",            // title
 			"You are about to be redirected out of this level! Are you sure you want leave?",   // content
 			"No", "Exit",      // buttons
-			[levelLayer](auto, bool btn2) mutable {
+			[=](auto, bool btn2) mutable {
 				if (btn2) {
-					// sends event here
+					log::debug("{}", input);
+					auto search = GJSearchObject::create(type, input);
+					// log::debug("{}", static_cast<int>(searchObj->m_searchType));
+					auto levelLayer = LevelBrowserLayer::scene(search);
+					CCDirector::sharedDirector()->replaceScene(CCTransitionFade::create(0.5f, levelLayer));
 				}
 			}
 		);		
@@ -46,78 +39,42 @@ void addSearchLayer(GJSearchObject* searchObj) {
 			"HOLD ON!",            // title
 			"You are about to be redirected out of the editor. Are you sure you don't want to save before exiting?",   // content
 			"No", "Exit",      // buttons
-			[](auto, bool btn2) {
+			[=](auto, bool btn2) {
 				if (btn2) {
-					// CCDirector::sharedDirector()->replaceScene(CCTransitionFade::create(0.5f, levelLayer));
+					auto search = GJSearchObject::create(type, input);
+					// log::debug("{}", static_cast<int>(searchObj->m_searchType));
+					auto levelLayer = LevelBrowserLayer::scene(search);
+					CCDirector::sharedDirector()->replaceScene(CCTransitionFade::create(0.5f, levelLayer));
 				}
-			}
-		);
-		} else {
-			CCDirector::sharedDirector()->pushScene(levelLayer);
-		}
-
-// onReplace: 
-// 	if (shouldReplace) {
-// 			CCDirector::sharedDirector()->replaceScene(CCTransitionFade::create(0.5f, levelLayer));
-// 		}
+			});
+	} else {
+		log::debug("x");
+		auto search = GJSearchObject::create(type, input);
+		auto levelLayer = LevelBrowserLayer::scene(search);
+		CCDirector::sharedDirector()->pushScene(levelLayer);
+	}
 }
 
+
 $on_mod(Loaded) {
+
 	handleURI("alert", [](std::string const& path) {
 		FLAlertLayer::create("Custom Alert", path, "Ok")->show();
 	});
 
 	// Level searching: TODO figure out correct sorting with links
 	handleURI("level", [](std::string const& path) {
-		GJSearchObject* x = GJSearchObject::create(SearchType::Search, path);
-		addSearchLayer(x);
-		// auto levelLayer = LevelBrowserLayer::scene(x);
-
-		// if (auto x = typeinfo_cast<PlayLayer*>(CCScene::get()->getChildren()->objectAtIndex(0))) {
-		// 	CCDirector::sharedDirector()->replaceScene(CCTransitionFade::create(0.5f, levelLayer));
-
-		// } else if (auto x = typeinfo_cast<LevelEditorLayer*>(CCScene::get()->getChildren()->objectAtIndex(0))) {
-		// 	geode::createQuickPopup(
-		// 		"HOLD ON!",            // title
-		// 		"You are about to be redirected out of the editor. Are you sure you want to save before exiting?",   // content
-		// 		"No", "Exit",      // buttons
-		// 		[levelLayer](auto, bool btn2) {
-		// 			if (btn2) {
-		// 				CCDirector::sharedDirector()->replaceScene(CCTransitionFade::create(0.5f, levelLayer));
-		// 			}
-		// 		}
-		// 	);
-		// } else {
-		// 	CCDirector::sharedDirector()->pushScene(levelLayer);
-		// }
+		addSearchLayer(SearchType::Search, path);
 	});
 
 	// Handles searching for players
 	handleURI("user", [](std::string const& path) {
-		GJSearchObject* x = GJSearchObject::create(SearchType::Users, path);
-		addSearchLayer(x);
-		// auto levelLayer = LevelBrowserLayer::scene(x);
-		// CCDirector::sharedDirector()->replaceScene(CCTransitionFade::create(.5f, levelLayer));
-		// CCDirector::
-		// auto scene = CCScene::get();
-		// levelLayer->setTouchPriority(-500);
-		// scene->addChild(levelLayer);
+		addSearchLayer(SearchType::Users, path);
 	});
 
-	// handleURI("search", [](std::string const& mode, std::string const& content) {
-	// 	std::string text = content;
-	// 	std::transform(text.begin(), text.end(), text.begin(), [](unsigned char c) {return std::tolower(c);});
-
-	// 	// switch (mode)
-	// 	// {
-	// 	// case constant expression:
-	// 	// 	/* code */
-	// 	// 	break;
-		
-	// 	// default:
-	// 	// 	break;
-	// 	// }
-	// });
+	handleURI("alert", [](std::string const& path) {
+		FLAlertLayer::create("Custom Alert", path, "Ok")->show();
+	});
 };
 
 
