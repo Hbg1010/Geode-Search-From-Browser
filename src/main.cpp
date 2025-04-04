77d62c01e7708f51d56d9612cbfb6144bb51bc8c
hbg1010 <harrisong26@parkschool.net>
2025-04-04 01:28:14 -0400

    list support + started refactor with settings

--- src/main.cpp
+++ src/main.cpp
@@ -41,49 +41,80 @@ class $modify(awesomeGM, GameManager) {
 	}
 };
 
-void addSearchLayer(SearchType type, const std::string& input) {
+void playSearch(SearchType type, const std::string& input, PlayLayer* pl = PlayLayer::get(), int searchMode) {
+	PauseLayer* pauseLayer = pl->getParent()->getChildByType<PauseLayer>(0);
+	if (!pauseLayer) pauseLayer = PauseLayer::create(false);
+	if (!pauseLayer) return;
+
+	auto search = GJSearchObject::create(type, input);
+	auto levelLayer = LevelBrowserLayer::scene(search);
+	awesomeGM* manager = static_cast<awesomeGM*>(GameManager::get());
+	manager->tempField(levelLayer);
+	pauseLayer->onQuit(nullptr);
+}
+
+void editorSearch(SearchType type, const std::string& input, LevelEditorLayer* lel = LevelEditorLayer::get(), int searchMode) {
+	EditorPauseLayer* editorPause = EditorPauseLayer::create(lel);
+	if (!editorPause) return;
+
+	auto search = GJSearchObject::create(type, input);
+
+	if (Mod::get()->getSettingValue<bool>("saveByDefault")) {
+		editorPause->onSaveAndPlay(nullptr);
+	} else {
+		editorPause->onExitNoSave(nullptr);
+	}
+
+	auto levelLayer = LevelBrowserLayer::scene(search);
+	CCDirector::sharedDirector()->pushScene(levelLayer);
+	// CCDirector::sharedDirector()->replaceScene(CCTransitionFade::create(0.5f, levelLayer));
+}
+
+
+void addSearchLayer(SearchType type, const std::string& input, int searchMode = 0) {
 	if (auto pl = PlayLayer::get()) {
 		if (!pl->m_isPaused || (pl->getParent() && !pl->getParent()->getChildByType<PauseLayer>(0))) UILayer::get()->onPause(nullptr);
-		// bool shouldReplace = false;
-		geode::createQuickPopup(
-			"HOLD ON!",            // title
-			"You are about to be redirected out of this level! Are you sure you want leave?",   // content
-			"No", "Exit",      // buttons
-			[=](auto, bool btn2) {
-				if (!btn2) return;
-				// log::debug("{}", input);
-				PauseLayer* pauseLayer = pl->getParent()->getChildByType<PauseLayer>(0);
-				if (!pauseLayer) pauseLayer = PauseLayer::create(false);
-				if (!pauseLayer) return;
+			if (Mod::get()->getSettingValue<bool>("plPopup")) {
+				geode::createQuickPopup(
+					"HOLD ON!",            // title
+					"You are about to be redirected out of this level! Are you sure you want leave?",   // content
+					"No", "Exit",      // buttons
+					[=](auto, bool btn2) {
+						if (btn2) playSearch(type, input, pl, searchMode);
+				});		
+			} else playSearch(type, input, pl, searchMode);
 
-				auto search = GJSearchObject::create(type, input);
-				auto levelLayer = LevelBrowserLayer::scene(search);
-				awesomeGM* manager = static_cast<awesomeGM*>(GameManager::get());
-				manager->tempField(levelLayer);
-				pauseLayer->onQuit(nullptr);
-			}
-		);		
 	} else if (auto lel = LevelEditorLayer::get()) {
-		geode::createQuickPopup(
-			"HOLD ON!",            // title
-			"You are about to be redirected out of the editor. Your changes will be saved!",   // content
-			"No", "Exit",      // buttons
-			[=](auto, bool btn2) {
-				if (!btn2) return;
-				EditorPauseLayer* editorPause = EditorPauseLayer::create(lel);
-				if (!editorPause) return;
-				editorPause->onSaveAndPlay(nullptr);
-				auto search = GJSearchObject::create(type, input);
-				auto levelLayer = LevelBrowserLayer::scene(search);
-				CCDirector::sharedDirector()->replaceScene(CCTransitionFade::create(0.5f, levelLayer));
-			}
-		);
+		if (Mod::get()->getSettingValue<bool>("editorPopup")) {
+			geode::createQuickPopup(
+				"HOLD ON!",            // title
+				"You are about to be redirected out of the editor. Your changes will be saved!",   // content
+				"No", "Exit",      // buttons
+				[=](auto, bool btn2) {
+					if (btn2) return editorSearch(type, input, lel, searchMode);
+				}
+			);
+		} else editorSearch(type, input, lel, searchMode);
+
 	} else {
 		auto search = GJSearchObject::create(type, input);
+		search->m_searchMode = searchMode;
 		auto levelLayer = LevelBrowserLayer::scene(search);
 		CCDirector::sharedDirector()->pushScene(levelLayer);
 	}
 }
+
+#include <Geode/modify/LevelBrowserLayer.hpp>
+class $modify(ieatfarts, LevelBrowserLayer) {
+	bool init(GJSearchObject* searchBy) {
+		if (!LevelBrowserLayer::init(searchBy)) return false;
+
+		log::debug("{}", static_cast<int>(searchBy->m_searchType));
+		log::debug("mode {}", static_cast<int>(searchBy->m_searchMode));
+
+		return true;
+	}
+};
 
 $on_mod(Loaded) {
 	// Level searching: TODO figure out correct sorting with links
@@ -94,5 +125,9 @@ $on_mod(Loaded) {
 	// Handles searching for players
 	handleURI("user", [](std::string const& path) {
 		addSearchLayer(SearchType::Users, path);
+	});
+
+	handleURI("list", [](std::string const& path) {
+		addSearchLayer(SearchType::Search, path, 1); // 1 is list mode
 	});
 };
